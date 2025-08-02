e4724ecd6ddd87a941ce8ec6b3d16064319ec9ab
naisanzaa <naisanza@gmail.com>
2025-08-02 02:28:29 -0400

    [scripts] run one or a bunch of them

--- run-all.sh
+++ run-all.sh
@@ -4,8 +4,16 @@
 
 images="latest alpine-i3 alpine-mate arch-i3 arch-kde arch-mate arch-xfce debian-i3 debian-kde debian-mate debian-xfce el-i3 el-mate el-xfce fedora-i3 fedora-kde fedora-mate fedora-xfce ubuntu-i3 ubuntu-kde ubuntu-mate ubuntu-xfce"
 
-docker run --rm \
-  -p 3000:3000 \
-  -p 3001:3001 \
-  --shm-size="2gb" \
-  lscr.io/linuxserver/webtop:ubuntu-mate
+for image in $images; do
+    
+    echo "[docker] :: running :: $image"
+    
+    docker run --rm \
+      -p 3000:3000 \
+      -p 3001:3001 \
+      --shm-size="2gb" \
+      lscr.io/linuxserver/webtop:$image
+      
+      sleep 1 
+
+done
