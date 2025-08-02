46ca669ceb43abfc79aa5007cbd926dc03432f5f
naisanzaa <naisanza@gmail.com>
2025-08-02 01:09:47 -0400

    [scripts] add all images to run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,14 @@
 
 # run
 
-docker run --rm \
-  -p 3000:3000 \
-  -p 3001:3001 \
-  --shm-size="2gb" \
-  lscr.io/linuxserver/webtop:arm64v8-arch-xfce
-  
+images="latest alpine-i3 alpine-mate arch-i3 arch-kde arch-mate arch-xfce debian-i3 debian-kde debian-mate debian-xfce el-i3 el-mate el-xfce fedora-i3 fedora-kde fedora-mate fedora-xfce ubuntu-i3 ubuntu-kde ubuntu-mate ubuntu-xfce"
+
+for image in $images; do
+    
+    docker run --rm \
+      -p 3000:3000 \
+      -p 3001:3001 \
+      --shm-size="2gb" \
+      lscr.io/linuxserver/webtop:$image
+
+done
